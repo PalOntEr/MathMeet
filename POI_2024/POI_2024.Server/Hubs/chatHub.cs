98c70a8ec08d4fa85bbf7f71e317cb52307f41a4
Roberto Carlos <RCDE11@Outlook.com>
2024-10-07 09:28:27 -0600

    Created the hubs folder and the chathub class using signalR

---
+++ POI_2024/POI_2024.Server/Hubs/chatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+
+namespace POI_2024.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
