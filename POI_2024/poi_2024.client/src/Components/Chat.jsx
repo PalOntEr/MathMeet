3bbc83222ec0879c1728300317a1a5a0b0616ff4
Roberto Carlos <RCDE11@Outlook.com>
2024-10-07 09:10:43 -0600

    Created and Added Chat Component to chats page

---
+++ POI_2024/poi_2024.client/src/Components/Chat.jsx
@@ -0,0 +1,78 @@
+﻿import { useState, useEffect } from 'react';
+import * as signalR from '@microsoft/signalr';
+import IncomingMessage from '../Components/IncomingMessage.jsx';
+import OutgoingMessage from '../Components/OutgoingMessage.jsx';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import SendIcon from '@mui/icons-material/Send';
+import AddIcon from '@mui/icons-material/Add';
+
+const Chat = () => {
+    const [messages, setMessages] = useState([]);
+    const [user, setUser] = useState('Roberto');
+    const [message, setMessage] = useState('');
+    const [connection, setConnection] = useState(null);
+   
+    useEffect(() => {
+       const connection = new signalR.HubConnectionBuilder()
+            .withUrl("/chatHub", {
+                withCredentials: true // If you are using cookies, enable this
+            })
+            .withAutomaticReconnect()
+            .build();
+
+        connection.start()
+            .then(() => {
+                console.log("Connected to SignalR hub");
+                setConnection(connection);
+            })
+            .catch(err => console.error("SignalR connection error: ", err));
+
+        connection.on("ReceiveMessage", (sender, receivedMessage) => {
+            setMessages(prevMessages => [...prevMessages, { message: receivedMessage, sender, type: "incoming" }]);
+        });
+
+        return () => {
+            connection.stop();
+        };
+    }, []);
+
+    const sendMessage = async () => {    
+        if (connection && message) { 
+            try {
+                await connection.invoke('SendMessage', user, message);
+                console.log("ola si se mando el mensaje");
+                setMessage('');
+            } catch (err) {
+                console.log("Mamaste: " + err);
+            }
+        }
+    };
+
+    return (
+        <div className="flex flex-col w-full h-full overflow-y-scroll">
+            <div id="Messages-Container" className="flex flex-col w-full h-full overflow-y-scroll">
+                {messages.map((msg, index) => (
+                    msg.type === "incoming" ?
+                        (<IncomingMessage key={index} message={msg.message} sender={msg.sender} /> ):
+                        (<OutgoingMessage key={index} message={msg.message} sender={msg.sender} />)
+                ))}
+            </div>
+            <div id="MessageInput-Container" className="flex h-[5%] justify-center items-center space-x-2">
+                <div id="btn-container" className="">
+                    <button id="AddFile"><AddIcon className=" text-primary" /></button>
+                </div>
+                <div className='btn-container'>
+                    <button id="Emojis"><EmojiEmotionsIcon className="Icon-container text-primary" /></button>
+                </div>
+                <div className="w-full">
+                    <input type="text" className="w-full bg-transparent border-b-2 border-[var(--primary-color)] text-white outline-none" id="Message" value={message} onChange={(e) => setMessage(e.target.value)}></input>
+                </div>
+                <div className='btn-container'>
+                    <button onClick={sendMessage} id="SendMessage"><SendIcon className="Icon-container text-primary" /> </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Chat;
--- POI_2024/poi_2024.client/src/Pages/Chats.jsx
+++ POI_2024/poi_2024.client/src/Pages/Chats.jsx
@@ -1,31 +1,17 @@
 import SideBar from '../Components/SideBar.jsx'
 import './Chat.css'
-import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 import GroupImg from '../Images/DAMN.png'
 import SendIcon from '@mui/icons-material/Send';
 import AddIcon from '@mui/icons-material/Add';
 import AssignmentIcon from '@mui/icons-material/Assignment';
 import VideocamIcon from '@mui/icons-material/Videocam';
 import IncomingMessage from '../Components/IncomingMessage.jsx';
+import Chat from '../Components/Chat.jsx';
 import OutgoingMessage from '../Components/OutgoingMessage.jsx';
 
 import { Link } from 'react-router-dom'
 
 function Chats() {
-
-    const messages = [
-        { message: "Hola", sender: "Roberto", type: "incoming" },
-        { message: "Hola", sender: "Max", type: "outgoing" },
-        { message: "👁️👁️", sender: "Max", type: "outgoing" },
-        { message: "e", sender: "Roberto", type: "incoming" },
-        { message: "pero", sender: "Roberto", type: "incoming" },
-        { message: "entonces si se sube", sender: "Roberto", type: "incoming" },
-        { message: "lo de", sender: "Roberto", type: "incoming" },
-        { message: "administracion", sender: "Roberto", type: "incoming" },
-        { message: "o ne", sender: "Roberto", type: "incoming" },
-        { message: "no se", sender: "Max", type: "outgoing" }
-    ];
-
     return (
         <div id="Content-container" className="flex h-screen w-2/3 xs:w-3/4">
             <div id="Chat-Container" className="flex flex-col justify-between h-full w-3/4 px-2">
@@ -34,27 +20,7 @@ function Chats() {
                         <VideocamIcon className="bg-primary p-px rounded-full"/>
                     </Link>
                 </div>
-                <div id="Messages-Container" className="flex flex-col w-full h-full overflow-y-scroll">
-                    {messages.map((msg, index) => (
-                        msg.type === "incoming" ? 
-                        <IncomingMessage key={index} message={msg.message} sender={msg.sender} /> : 
-                        <OutgoingMessage key={index} message={msg.message} sender={msg.sender} />
-                    ))}
-                </div>
-                <div id="MessageInput-Container" className="flex h-[5%] justify-center items-center space-x-2">
-                    <div id="btn-container" className="">
-                        <button id="AddFile"><AddIcon className=" text-primary" /></button>
-                    </div>
-                    <div className='btn-container'>
-                        <button id="Emojis"><EmojiEmotionsIcon className="Icon-container text-primary" /></button>
-                    </div>
-                    <div className="w-full">
-                        <input type="text" className="w-full bg-transparent border-b-2 border-[var(--primary-color)] text-white outline-none" id="Message"></input>
-                    </div>
-                    <div className='btn-container'>
-                        <button id="SendMessage"><SendIcon className="Icon-container text-primary" /> </button>
-                    </div>
-                </div>
+                <Chat /> 
             </div>
 
             <div className="ChatInfo-Container container w-1/4 h-full bg-comp-1 px-4">
