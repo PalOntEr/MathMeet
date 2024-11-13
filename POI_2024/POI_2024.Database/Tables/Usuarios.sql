908ad6cc2a59bb13bc6214058081d9cad79c5ecb
Roberto Carlos <RCDE11@Outlook.com>
2024-11-12 22:39:19 -0600

    Added Encrypted and Status column

--- POI_2024/POI_2024.Database/Tables/Usuarios.sql
+++ POI_2024/POI_2024.Database/Tables/Usuarios.sql
@@ -4,6 +4,8 @@ CREATE TABLE [dbo].[Usuarios] (
     [Contrasena]     VARCHAR (150) NOT NULL,
     [ID_ArchivoFoto] INT           NULL,
     [CalCoins]       INT           DEFAULT ((0)) NOT NULL,
+    [Status] INT NULL, 
+    [Encrypt] BIT NULL, 
     PRIMARY KEY CLUSTERED ([Matricula] ASC),
     FOREIGN KEY ([ID_ArchivoFoto]) REFERENCES [dbo].[Archivos] ([ID_Archivo])
 );
