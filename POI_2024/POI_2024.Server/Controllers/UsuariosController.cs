c20835ffbed036675d2653a8c412795b349f68ad
Roberto Carlos <RCDE11@Outlook.com>
2024-11-12 22:35:44 -0600

    Added Put Method to update the user info

--- POI_2024/POI_2024.Server/Controllers/UsuariosController.cs
+++ POI_2024/POI_2024.Server/Controllers/UsuariosController.cs
@@ -17,7 +17,6 @@ namespace POI_2024.Server.Controllers
         }
 
         [HttpGet(Name = "GetUsuarios")]
-
         public async Task<ActionResult<IEnumerable<Usuario>>> Get([FromQuery] string? name)
         {
             if (!string.IsNullOrWhiteSpace(name))
@@ -36,6 +35,57 @@ namespace POI_2024.Server.Controllers
                 u.Matricula
             }).ToListAsync();
             return Ok(Users);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] Usuario UsuarioToUpdate)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var UsuarioFound = await _context.Usuarios.Where(u => u.Matricula == UsuarioToUpdate.Matricula).FirstOrDefaultAsync();
+            Console.WriteLine(UsuarioFound);
+            if (UsuarioFound == null)
+            {
+                return NotFound("User not found");
+            }
+
+            // Update only the fields that are not null
+            if (!string.IsNullOrEmpty(UsuarioToUpdate.NombreCompleto))
+            {
+                UsuarioFound.NombreCompleto = UsuarioToUpdate.NombreCompleto;
+            }
+
+            if (!string.IsNullOrEmpty(UsuarioToUpdate.Contrasena))
+            {
+                UsuarioFound.Contrasena = UsuarioToUpdate.Contrasena;
+            }
+
+            if (UsuarioToUpdate.ID_ArchivoFoto != 0)
+            {
+                UsuarioFound.ID_ArchivoFoto = UsuarioToUpdate.ID_ArchivoFoto;
+            }
+
+            if (UsuarioToUpdate.CalCoins != 0)
+            {
+                UsuarioFound.CalCoins += UsuarioToUpdate.CalCoins;
+            }
+
+            if (UsuarioToUpdate.Encrypt != null)
+            {
+                UsuarioFound.Encrypt = UsuarioToUpdate.Encrypt;
+            }
+
+            if (UsuarioToUpdate.status != null)
+            {
+                UsuarioFound.status = UsuarioToUpdate.status;
+            }
+            // Save changes to the database
+            await _context.SaveChangesAsync();
+
+            return Ok("User information updated successfully");
         }
     }
 }
